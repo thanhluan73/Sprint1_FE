e067f471d9d638dff677826df3a4a2f91ed8efc2
thanhluan73 <thanhluan73@Las-MacBook-Pro.local>
2018-10-18 16:35:36 +0700

    update giao dien

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Login from 'pages/login/Login';
 import Company from 'pages/manager/company/Company';
+import ForgotPassword from './pages/forgot-password/ForgotPassword';
+import RequestPassword from './pages/request-password/RequestPassword';
+import RequestRegister from './pages/request-register/RequestRegister';
 import Users from 'pages/manager/user/Users';
+
 export const adminRoutes = [
     {
         path: '/companys',
@@ -21,5 +25,20 @@ export const publicRoutes=[
         exact: true,
         main: () => <Login />
     },
+    {
+        path: '/forgotpassword',
+        exact: true,
+        main: () => <ForgotPassword/>
+    },
+    {
+        path: '/requestpassword',
+        exact: true,
+        main: () => <RequestPassword />
+    },
+    {
+        path: '/requestregister',
+        exact: true,
+        main: () => <RequestRegister />
+    }
 ]
 
